3823e69ede73cf1ed1fc51672f7c962695c9f012
k33ngTomide <Msodiqakintomide@gmail.com>
2024-02-14 15:10:25 +0100

    Added a new cluster

--- src/main/java/com/acaclan/aceclan/AceClanConfig/ClanConfig.java
+++ src/main/java/com/acaclan/aceclan/AceClanConfig/ClanConfig.java
@@ -1,7 +1,6 @@
-package com.acaclan.aceclan;
+package com.acaclan.aceclan.AceClanConfig;
 
 import org.modelmapper.ModelMapper;
-import org.modelmapper.internal.bytebuddy.matcher.ModifierMatcher;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
--- src/main/java/com/acaclan/aceclan/AceclanApplication.java
+++ src/main/java/com/acaclan/aceclan/AceclanApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AceclanApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(AceclanApplication.class, args);
 	}
